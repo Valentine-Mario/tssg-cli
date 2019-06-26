012ea45560ffd708ea3ba158537b4f672d6e553f
valentine-mario <valentine13400@gmail.com>
2019-06-26 14:10:51 +0100

    added readme

---
+++ README.md
@@ -0,0 +1,31 @@
+# Ts-generate-cli
+Cli tool that helps you generate typrscript for you server side development plus all the configurations
+
+Install the package globally with the command 
+
+```
+npm i ts-generate-cli -g
+
+```
+
+## Usage
+
+* To view version, run the command 
+
+```
+tssg -v
+
+```
+* To create app, run the command 
+
+```
+tssg -c <dir name>
+
+```
+or 
+
+```
+tssg --create <dir name>
+
+```
+If you fail to supply a dir name, it uses the app as the default name
--- tssg.js
+++ tssg.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+// A simple cli instance to create a typescript setup for server side using ts-Node, and express js
 var program = require('commander');
 var readline = require('readline')
 const fse = require('fs-extra')
@@ -7,7 +8,8 @@ const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
   });
-  
+var path = require('path')  
+const TEMPLATE_DIR = path.join(__dirname,  'setup')
 
 program
   .version('0.1.0', '-v, --version')
@@ -18,9 +20,17 @@ program
   
 if(program.create) {
   try{
-    fse.copySync('./setup', `./${`-%s create`, program.create}`)
+    fse.copySync(TEMPLATE_DIR, `./${`-%s create`, program.create}`)
+    console.log(`create ${`-%s create`, program.create}/package.json`)
+    console.log(`created ${`-%s create`, program.create}/tsconfig.json`)
+    console.log(`created ${`-%s create`, program.create}/src/app.ts`)
+    console.log(`created ${`-%s create`, program.create}/src/bin/www.ts`)
+    console.log(`created ${`-%s create`, program.create}/src/routes/index.ts`)
     console.log(`cd ${`-%s create`, program.create}`);
     console.log(`npm install`)
+    console.log(`npm run build to run build`)
+    console.log(`npm start to start typescript app`)
+    console.log(`npm run dev to run javascript after build`)
   }catch(err){
     console.log(err)
   }    
